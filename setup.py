634b31eb4194dd36a8c03681442e09d198297902
Abdelaziz Khabthani <abdelaziz.khabthani@gmail.com>
2021-02-27 15:01:06 +0100

    Better setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 """ setup.py definition file """
-
 from os import path
 from setuptools import setup, find_packages
 
 this_directory = path.abspath(path.dirname(__file__))
+
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as readme_file:
     long_description = readme_file.read()
+
+with open(path.join(this_directory, 'requirements', 'common.txt')) as common_requirement_file:
+    install_requires = common_requirement_file.read().splitlines()
 
 setup(
     name='rpi_gpio_project_template',
@@ -15,13 +18,15 @@ setup(
     author='Abdelaziz Khabthani',
     author_email='abdelaziz.khabthani@gmail.com',
     entry_points={
-        'console_scripts': ['rpi_gpio_project_template=rpi_gpio_project_template.rpi_gpio_project_template:main']
+        'console_scripts': [
+            'rpi_gpio_project_template=rpi_gpio_project_template.rpi_gpio_project_template:main'
+        ]
     },
     packages=find_packages(
-        include=['rpi_gpio_project_template', 'rpi_gpio_project_template.*']
+        include=[
+            'rpi_gpio_project_template',
+            'rpi_gpio_project_template.*'
+        ]
     ),
-    install_requires=[
-        'RPi.GPIO==0.7.0',
-        'smbus2==0.4.1'
-    ]
+    install_requires=install_requires
 )
