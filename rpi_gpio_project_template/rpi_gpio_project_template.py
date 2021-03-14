d8a50b81fdbf22e0fffd975cde81e5c4c047199a
Abdelaziz Khabthani <abdelaziz.khabthani@gmail.com>
2021-03-14 17:22:49 +0100

    Adding delays

--- rpi_gpio_project_template/rpi_gpio_project_template.py
+++ rpi_gpio_project_template/rpi_gpio_project_template.py
@@ -1,20 +1,26 @@
 """ A template for entry point class to be used with RPi.GPIO """
 import atexit
+import time
 import RPi.GPIO as GPIO
 
 
 class RpiGpioProjectTemplate:
     """ Main class program """
 
-    def __init__(self):
+    def __init__(self, init_delay = 0, loop_delay = 0):
         """ Insance fields definition """
+        self._init_delay = init_delay
+        self._loop_delay = loop_delay
+        time.sleep(self._init_delay)
 
     def _setup(self):
         """ Setup function (Will execute only one time) """
         atexit.register(self._clean)
+        time.sleep(self._init_delay)
 
     def _loop(self):
         """ Loop function (Will execute every tick) """
+        time.sleep(self._loop_delay)
 
     def _clean(self):
         """ Clean up function (Will execute when program exits) """
@@ -34,7 +40,7 @@ class RpiGpioProjectTemplate:
 
 def main():
     """ Global function to initiate the module (used for setup.py as an enttry point) """
-    RpiGpioProjectTemplate().start()
+    RpiGpioProjectTemplate(init_delay=1, loop_delay=0.025).start()
 
 
 if __name__ == "__main__":
